1e56596fcb7a1c1e0868dce55e55dc599dc62f5c
Nicola Tonellotto <nicola.tonellotto@gmail.com>
2022-03-11 10:27:32 +0000

    working

--- drhard/star/index.py
+++ drhard/star/index.py
@@ -6,19 +6,22 @@ import faiss
 
 import numpy as np
 
+from tqdm import tqdm
+from pathlib import Path
 
-def construct_flatindex_from_embeddings(embeddings, ids=None):
+def construct_flatindex_from_embeddings(embeddings, ids=None, bs=32):
     dim = embeddings.shape[1]
-    # print('embedding shape: ' + str(embeddings.shape))
-    # index = faiss.index_factory(dim, "Flat", faiss.METRIC_INNER_PRODUCT)
     index = faiss.IndexFlatIP(dim)
     if ids is not None:
         ids = ids.astype(np.int64)
-        # print(ids.shape, ids.dtype)
         index = faiss.IndexIDMap2(index)
-        index.add_with_ids(embeddings, ids)
+        for batchpos in tqdm(range(0, len(embeddings), bs)):
+            endpos = min(batchpos + bs, len(embeddings))
+            index.add_with_ids(embeddings[batchpos:endpos], ids[batchpos:endpos])
     else:
-        index.add(embeddings)
+        for batchpos in tqdm(range(0, len(embeddings), bs)):
+            endpos = min(batchpos + bs, len(embeddings))
+            index.add(embeddings[batchpos:endpos])
     return index
 
 
@@ -27,17 +30,18 @@ def main():
     parser.add_argument("--de", type=str, required=True)
     parser.add_argument("--di", type=str, required=True)
     parser.add_argument("--out", type=str, required=True)
+    parser.add_argument("--bs",  type=int, default=1024)
     args = parser.parse_args()
 
     if os.path.exists(args.out):
-        print(f"indexed data already exist in {args.out}, exit indexing")
+        print(f"indexed data already exist: {args.out}, exit indexing")
     else:
-        os.makedirs(args.out, exist_ok=True)
+        os.makedirs(Path(args.out).parent, exist_ok=True)
         doc_ids        = np.memmap(args.di, dtype=np.int32,   mode="r")
         doc_embeddings = np.memmap(args.de, dtype=np.float32, mode="r")
         doc_embeddings = doc_embeddings.reshape(len(doc_ids), -1)
 
-        index = construct_flatindex_from_embeddings(doc_embeddings, doc_ids)
+        index = construct_flatindex_from_embeddings(doc_embeddings, doc_ids, args.bs)
 
         faiss.write_index(index, args.out)
 
